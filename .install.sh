8093ae5155645712470715939da20024870527b1
Anton Sivertsson <sivertsson.anton@gmail.com>
2025-12-22 16:47:59 +0100

    Add simple install script

---
+++ .install.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+# Helpers
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+echo " MacOS setup"
+
+xcode-select --install
+
+# Setting up homebrew
+
+if ! command_exists brew; then
+  echo "🍺 Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew analytics off
+else
+  echo "🍺 Homebrew already installed"
+fi
+
+# Tap required formulae
+brew tap FelixKratz/formulae
+
+echo "Installing homebrew packages..."
+brew bundle --file "$HOME/.setup_brewfile"
+echo "Done!"
