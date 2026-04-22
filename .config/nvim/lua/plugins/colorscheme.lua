ce0eb4b7baaf78dc85f1000fe2b304d23f0fc4dc
Anton Sivertsson <sivertsson.anton@gmail.com>
2026-04-22 13:51:15 +0200

    neovim - remove transparent

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,21 @@
-local function set_normal_float_highlight()
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-  vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
-  vim.api.nvim_set_hl(0, "FloatTitle", { bg = "NONE" })
-end
+-- local function set_normal_float_highlight()
+--   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+--   vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+--   vim.api.nvim_set_hl(0, "FloatTitle", { bg = "NONE" })
+-- end
 
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = set_normal_float_highlight,
-})
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+--   pattern = "*",
+--   callback = set_normal_float_highlight,
+-- })
 
 return {
   {
     "neanias/everforest-nvim",
     config = function()
-      require("everforest").setup({
-        transparent_background_level = 2,
-      })
+      -- require("everforest").setup({
+      --   transparent_background_level = 2,
+      -- })
     end,
   },
 
